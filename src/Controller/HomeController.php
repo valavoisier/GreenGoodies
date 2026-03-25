e04db938548468fc9eee8c0406829b8b5c5bc443
Valerie Lavoisier <val.lavoisier3@gmail.com>
2026-03-25 10:33:24 +0100

    feat: création ProductController + page produit (show) avec données dynamiques pour homepage et page produit - Ajout du lien depuis le menu et les cards produits vers la page produit (show)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $products = $productRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+        'products' => $products,
+    ]);
     }
 }
---
+++ src/Controller/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProductController extends AbstractController
+{
+     #[Route('/produits/{id}', name: 'app_product_show', requirements: ['id' => '\d+'])]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
