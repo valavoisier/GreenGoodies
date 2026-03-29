08db5193b946a04715d7133bbc2339c52c67c1a4
Valerie Lavoisier <val.lavoisier3@gmail.com>
2026-03-29 10:59:30 +0200

    feat: récupération des commandes utilisateur dans Mon compte - Sécurisation route avec IsGranted ROLE_USER - Récupération des commandes via relation User->Orders - Tri par date décroissante (plus récentes en premier) - Passage des commandes au template

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class AccountController extends AbstractController
 {
     #[Route('/account', name: 'app_account')]
+    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
+        // Récupération de l'utilisateur connecté
+        $user = $this->getUser();
+        
+        // Récupération des commandes de l'utilisateur
+        $orders = $user->getOrders();
+        
+        // Conversion en tableau pour tri
+        $ordersArray = $orders->toArray();
+        
+        // Tri par date décroissante (plus récentes en premier)
+        usort($ordersArray, function($a, $b) {
+            return $b->getCreatedAt() <=> $a->getCreatedAt();
+        });
+        
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'orders' => $ordersArray,
         ]);
     }
 }
